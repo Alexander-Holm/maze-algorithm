2cd620b5c054607e55b6335e3f72c970d6b20e08
Alexander Holm <alho0014@student.hv.se>
2022-03-06 00:58:38 +0100

    gh-pages

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+import { publish } from "gh-pages"
+
+publish(
+	'public', // path to public directory
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/Alexander-Holm/maze-algorithm.git', // Update to point to your repository
+		user: {
+			name: 'Alexander-Holm', // update to use your name
+			email: 'alex.holm@live.com' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
