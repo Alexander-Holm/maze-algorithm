0fe50d760cf9b678e8d034bd052c2a21b52eaba7
Alexander Holm <alho0014@student.hv.se>
2022-07-14 12:02:18 +0200

    hover och active effekter

--- src/components/ButtonSmall.svelte
+++ src/components/ButtonSmall.svelte
@@ -1,13 +1,13 @@
 <script>
-    export let disabled = false;
     export let content;
     export let contentStyle = "";
+    export let disabled = false;
 </script>
 
 <button
-        on:click
-        on:mousedown on:mouseup on:mouseleave
+        on:click on:mousedown on:mouseup on:mouseleave
         on:keydown on:keyup
+        on:blur
         disabled={disabled}
     >
         <span style={contentStyle} >
@@ -17,38 +17,34 @@
 
 <style>
     button{
-    background: transparent;
-    height: 1.5em;
-    width: 1.5em;
+    background-color: transparent;
+    height: 1.7em;
+    width: 1.7em;
     border-radius: 50%;
     border: 0;
 
     margin: 3px;
     padding: 0;
+    overflow: hidden;
+    transition-property: background-color, box-shadow;
+    transition-duration: 0.1s;
+
 }
+    button:focus-visible:enabled{
+        outline: 2px solid var(--accent-color);
+    }
     button:hover:enabled{
         /* Grå med låg opacity syns på både ljus och mörk bakgrund */
         background-color: hsla(0, 0%, 60%, 0.15);
         transform: scale(1.15);
     }
-    button:focus-visible{
-        outline: 2px solid var(--accent-color);
-    }
-    button:active{
-        filter: contrast(1.1);
+    button:active:enabled{
+        box-shadow: 0 0 2px hsla(0, 0%, 50%, 0.5) inset;
     }
     span{
         display: inline-block;
         height: 100%;
         width: 100%;
-        /* 
-            Unicode ikoner är inte alltid centrerade i höjd (har mer whitespace över eller under).            
-            Det här centrerar dom nuvarande hyffsat bra i höjd oavsett font-size,
-            men jag tror inte det löser problemet i alla lägen.
-
-            Kan använda transform:translate() i contentStyle för finjustering.
-        */
-        line-height: 1.3em;
-        vertical-align: super;
+        pointer-events: none;
     }
 </style>
