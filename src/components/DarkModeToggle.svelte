52cac8599328aaa77665fdbfe8fc536f061d051b
Alexander Holm <alho0014@student.hv.se>
2022-07-26 10:31:50 +0200

    Ändrat ikoner och färg

--- src/components/DarkModeToggle.svelte
+++ src/components/DarkModeToggle.svelte
@@ -1,18 +1,14 @@
 <script>
-    // Ikonerna är .svg men filtypen är ändrad till .svelte för att det 
-    // är det lättaste sättet att använda dom.
-    import Sun from "../icons/sun.svelte";
-    import Moon from "../icons/moon.svelte";
-    
+    import {Sun, Moon} from "../icons/darkmode"
+
     const browserPreference = window.matchMedia("(prefers-color-scheme: dark)").matches;
     let darkMode = browserPreference;
     $: document.documentElement.setAttribute("data-dark-mode", darkMode);
 </script>
 
-<button id="dark-mode-toggle" on:click={() => darkMode = !darkMode}>
-    <span class="background" />
-    <span class="icon" ><Sun/></span>
-    <span class="icon" ><Moon/></span>   
+<button id="dark-mode-toggle" class:dark={darkMode} on:click={() => darkMode = !darkMode}>
+    <span class="icon" >{@html Sun}</span>
+    <span class="icon" >{@html Moon}</span>   
     <span class="selected-indicator" class:right={darkMode} />
     <!-- 
         Eget element för shadow så att det kan ha högre z-index än ikonerna
@@ -29,9 +25,9 @@
         top: 50%; right: -20px;
         transform: translateY(-50%);
         /* font-size bestämmer storleken på allt */
-        font-size: 1.9rem;
+        font-size: 2rem;
         height: 1em;
-        width: 2.3em;        
+        width: 2.2em;        
         box-shadow: 0 0 2px 1px gray;  
         border-radius: 999px;
 
@@ -39,36 +35,33 @@
         justify-content: space-between;
         align-items: center;
 
-        background-color: transparent;
         padding: 0;
         margin: 0;
         border: 0;
-        outline: 2px solid transparent;
+
+        /* Flyttar bakgrunden för att kunna ha transition med gradient */
+        background: linear-gradient(90deg, rgb(197, 250, 255) 20%, rgb(158, 135, 187) 80%);
+        background-size: 150%;
+        background-position: left;
 
         /* quartic-out */
-        transition: 0.2s cubic-bezier(.17,.84,.44,1);
-    }  
-        #dark-mode-toggle:hover,
+        transition: background-position 1s cubic-bezier(.17,.84,.44,1);
+
+    }    
+        #dark-mode-toggle.dark, #dark-mode-toggle:hover{
+            background-position: right;
+        }
+        #dark-mode-toggle, #dark-mode-toggle.dark:hover{
+            background-position: left;
+        }
         #dark-mode-toggle:focus-visible{
             outline-color: var(--accent-color);
             outline-offset: 8px;
         }
-        #dark-mode-toggle:active{
-            outline-offset: 6px;
-        }
-    .background{
-        background: linear-gradient(90deg, rgb(197, 250, 255) 0%, rgb(158, 135, 187) 100%);
-        position: absolute;
-        top: 0; left: 0;
-        width: 100%;
-        height: 100%;        
-        border-radius: inherit;
-        
-    }
     .icon{
         z-index: 2;
         display: flex;
-        padding: 0.14em;
+        padding: 0.2em;
         box-sizing: border-box;
     }
     
@@ -88,15 +81,20 @@
         top: 0; left: 0;
         transition: 0.2s ease;
     }
+    
     .selected-indicator{ background-color: white; }
+
     .selected-indicator-shadow{ 
-        box-shadow: 0 0 4px 1px black; 
         z-index: 3;
+        box-shadow: 0 0 4px 1px black;
     }
         .selected-indicator.right,
         .selected-indicator-shadow.right
         {
             left: 100%;
             transform: translateX(-100%);
+        }
+        #dark-mode-toggle:active .selected-indicator-shadow{
+            box-shadow: 0 0 6px 1px black;
         }
 </style>
---
+++ src/icons/darkmode.js
@@ -0,0 +1,29 @@
+// Ikoner från https://www.svgrepo.com/ men modifierade.
+// Har tagit bort några path som fungerade likt stroke och lagt till vanlig stroke.
+// Egna färger i fill.
+
+export const Sun = `
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 483.512 483.512" xml:space="preserve">
+        <polygon 
+            fill=#fdda20 stroke="black" stroke-width="30"
+            points="475.512,241.691 421.2,289.571 444.016,358.283 372.952,372.267 358.336,443.203 
+            289.76,419.915 241.752,474.131 193.752,419.915 125.168,443.203 110.552,372.259 39.488,358.275 62.312,289.563 8,241.691 
+            62.312,193.819 39.488,125.107 110.56,111.123 125.176,40.187 193.752,63.467 241.752,9.259 289.76,63.475 358.336,40.187 
+            372.952,111.131 444.024,125.115 421.2,193.827 "
+        />
+        <circle 
+            fill=rgb(255,239,0) stroke="black" stroke-width="20"
+            cx="241.752" cy="244.339" r="133.64"
+        />
+    </svg>
+`
+// viewBox är ändrad för att göra ikonen lite större, orginal är "0 0 24 24"
+export const Moon = `
+    <svg viewBox="0 1 22 22" xmlns="http://www.w3.org/2000/svg">
+        <path 
+            stroke=black stroke-width=1.5 fill=#ffef00
+            d="M3 17C10.952 18.6176 16.6829 8.75775 11 3C16.0007 3.13144 20 7.11149 20 12C20 16.9715 16.1188 21 11 21C7.77111 21 4.65938 19.4319 3 17Z"
+        />
+    </svg>
+`
